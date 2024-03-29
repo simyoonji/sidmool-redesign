d821383fd57a241416f3ddc8862484cb4b49372c
sim yoonji <124511094+simyoonji@users.noreply.github.com>
2024-03-29 19:08:09 +0900

    sdfsdfsd

--- script/scroll-animation.js
+++ script/scroll-animation.js
@@ -1,5 +1,10 @@
 const box1 = document.querySelector(".introduce");
 const box2 = document.querySelector(".advertise");
+const boxList1 = box2.querySelector('.advertise-list li:nth-of-type(1)');
+const boxList2 = box2.querySelector('.advertise-list li:nth-of-type(2)');
+const boxList3 = box2.querySelector('.advertise-list li:nth-of-type(3)');
+const boxList4 = box2.querySelector('.advertise-list li:nth-of-type(4)');
+
 const box3 = document.querySelector(".recommend");
 const box4 = document.querySelector(".event");
 const box5 = document.querySelector(".Campaign");
@@ -14,12 +19,36 @@ const box6 = document.querySelector(".custom");
 	}
 });
 
-gsap.to(box2, {
-	duration: 2,
+gsap.to(boxList1, {
+	duration: 1,
 	opacity : 1,
 
 	scrollTrigger: {
-		trigger : box2
+		trigger : boxList1
+	}
+});
+gsap.to(boxList2, {
+	duration: 1,
+	opacity : 1,
+
+	scrollTrigger: {
+		trigger : boxList2
+	}
+});
+gsap.to(boxList3, {
+	duration: 1,
+	opacity : 1,
+
+	scrollTrigger: {
+		trigger : boxList3
+	}
+});
+gsap.to(boxList4, {
+	duration: 1,
+	opacity : 1,
+
+	scrollTrigger: {
+		trigger : boxList4
 	}
 });
 
